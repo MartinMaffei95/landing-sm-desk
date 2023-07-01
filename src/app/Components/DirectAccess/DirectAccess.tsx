97ba63dc180994965e5ef23536bf0499b86def64
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-07-01 11:39:48 -0300

    added head GTM

--- src/app/Components/DirectAccess/DirectAccess.tsx
+++ src/app/Components/DirectAccess/DirectAccess.tsx
@@ -1,40 +1,40 @@
-'use client';
-import { useState } from 'react';
-import { Inter } from 'next/font/google';
-import { useRouter, usePathname } from 'next/navigation';
-import Wicon from '@/app/Components/WIcon/Wicon';
-import useMediaQuery from '@/hooks/useMediaQuery';
-import { mq } from '@/config/mediaQueries';
-const inter = Inter({ subsets: ['latin'] });
+'use client'
+import { useState } from 'react'
+import { Inter } from 'next/font/google'
+import { useRouter, usePathname } from 'next/navigation'
+import Wicon from '@/app/Components/WIcon/Wicon'
+import useMediaQuery from '@/hooks/useMediaQuery'
+import { mq } from '@/config/mediaQueries'
+const inter = Inter({ subsets: ['latin'] })
 
 type Props = {
-  name: string;
-  icon?: string;
-  route: string;
+  name: string
+  icon?: string
+  route: string
   //   open?: boolean;
-};
+}
 const DirectAccess = ({ name, icon, route = '/' }: Props) => {
-  let router = useRouter();
-  const pathname = usePathname();
-  const { isPhone, isTablet } = useMediaQuery(mq);
-  const [select, setSelect] = useState<boolean>(false);
-  const [open, setOpen] = useState<boolean>(pathname === route);
+  let router = useRouter()
+  const pathname = usePathname()
+  const { isPhone, isTablet } = useMediaQuery(mq)
+  const [select, setSelect] = useState<boolean>(false)
+  const [open, setOpen] = useState<boolean>(pathname === route)
   const handleSelect = () => {
-    setSelect(!select);
-  };
+    setSelect(!select)
+  }
   const handleOpen = () => {
-    setOpen(!open);
-  };
+    setOpen(!open)
+  }
 
   const openFolder = () => {
-    router.push(`${route}`);
-    handleOpen();
-  };
+    router.push(`${route}`)
+    handleOpen()
+  }
   return (
     <div
       onClick={isPhone || isTablet ? openFolder : handleSelect}
       onDoubleClick={openFolder}
-      className={`flex flex-col justify-center items-center  ${
+      className={`flex flex-col justify-center items-center menu-navigation-item  ${
         select ? 'bg-neutral-300 border-blue-400 border' : 'none'
       } bg-opacity-50 border-none px-4 focus-visible:bg-red-500`}
     >
@@ -58,7 +58,7 @@ const DirectAccess = ({ name, icon, route = '/' }: Props) => {
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default DirectAccess;
+export default DirectAccess
