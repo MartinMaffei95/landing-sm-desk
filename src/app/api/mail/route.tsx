b654870775cc0f5293817814e1ef0348b146055b
Martin Maffei <martinmaffei95@gmail.com>
2023-08-11 10:51:40 -0300

    feat. added the wp icon and finished the contact form

--- src/app/api/mail/route.tsx
+++ src/app/api/mail/route.tsx
@@ -1,5 +1,8 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { createTransport } from 'nodemailer'
+import { htmlTemplate } from '@/app/api/mail/template/create-template';
+import { validateContactRequest } from '@/app/api/validations/mail.validate';
+import { ContactData } from '@/interfaces';
+import { NextRequest, NextResponse } from 'next/server';
+import { createTransport } from 'nodemailer';
 
 // export async function POST(req: Request) {
 //   console.log(req);
@@ -8,29 +11,51 @@ import { createTransport } from 'nodemailer'
 
 //   return NextResponse.json(proccesedData);
 // }
-export async function POST(req: NextRequest, res: NextResponse) {
-  const transporter = createTransport({
-    port: 465,
-    host: 'smtp.gmail.com',
-    auth: {
-      user: 'demo@demo.gmail',
-      pass: 'password',
-    },
-    secure: true,
-  })
-  console.log('rq.BODY', req.body)
 
-  // const mailData = {
-  //   from: 'demo email',
-  //   to: 'your email',
-  //   subject: `Message From ${req?.body?.name}`,
-  //   text: req?.body.message + ' | Sent from: ' + req?.body.email,
-  //   html: `<div>${req?.body.message}</div><p>Sent from:
-  //   ${req?.body.email}</p>`,
-  // }
-  // transporter.sendMail(mailData, function (err, info) {
-  //   if (err) console.log(err)
-  //   else console.log(info)
-  // })
-  return NextResponse.json('ok')
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const request: ContactData = await req.json();
+    const schema = validateContactRequest();
+    const { company, country, firstName, mail, message, phone, secondName } =
+      schema.parse(request);
+
+    const transporter = createTransport({
+      port: 465,
+      host: 'smtp.gmail.com',
+      auth: {
+        user: 'mmaffei@socialmedia1989.com.ar',
+        pass: 'dyntcvramzjirwhs',
+      },
+      secure: true,
+      options: {
+        from: 'noanswer@noanswer.com',
+      },
+    });
+
+    const mailData = {
+      from: 'noreply@socialmedia1989.com.ar <donotreply@bar.com>',
+      to: 'josemensi@socialmedia1989.com.ar',
+      subject: `Nuevo contacto de ${firstName} ${secondName} ${
+        company.length > 0 ? ' | ' + company : null
+      }`,
+      text: message + ' | Enviado por: ' + mail,
+      html: htmlTemplate({
+        company,
+        country,
+        firstName,
+        mail,
+        message,
+        phone,
+        secondName,
+      }),
+    };
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) {
+        throw new Error();
+      }
+    });
+    return new Response('SUCCESS', { status: 200 });
+  } catch (error) {
+    return new Response('ERROR', { status: 500 });
+  }
 }
